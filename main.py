a1201bdc4d4e6777c7d379daff43635cd56a518b
Junaid-Zulfiqar <junaidzulfiqar377@gmail.com>
2022-06-17 17:40:25 +0500

    udpate everything

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import os
-# import time
-# import cv2
-# import mediapipe as mp
-# import numpy as np
-# import base64
+import time
+import cv2
+import mediapipe as mp
+import numpy as np
+import base64
 
 #load fastapi
 app = FastAPI()
@@ -31,31 +31,31 @@ def read_root():
     return {"Hello": "World"}
 
 #post function
-# @app.post("/person_dectector")
-# async def upload_base64(item:Item):
-#     mpFaceDetection = mp.solutions.face_detection
-#     mpDraw = mp.solutions.drawing_utils
-#     faceDetection = mpFaceDetection.FaceDetection(model_selection=2,min_detection_confidence=0.4)
-#     encoded_data = item.url.split(',')[1]
-#     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#     results = faceDetection.process(img)
-#     if results.detections:
-#         if len(results.detections) == 1:
-#           return {   
-#             'person':True,
-#             'text':"Single person found"
-#             }  
-#         else:
-#           return {   
-#             'person':False,
-#             'text':"Multiple persons are existed"
-#             }      
-#     else:
-#         return {   
-#         'person':False,
-#         'text':"no one is found in picture"
-#         }      
+@app.post("/person_dectector")
+async def upload_base64(item:Item):
+    mpFaceDetection = mp.solutions.face_detection
+    mpDraw = mp.solutions.drawing_utils
+    faceDetection = mpFaceDetection.FaceDetection(model_selection=2,min_detection_confidence=0.4)
+    encoded_data = item.url.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    results = faceDetection.process(img)
+    if results.detections:
+        if len(results.detections) == 1:
+          return {   
+            'person':True,
+            'text':"Single person found"
+            }  
+        else:
+          return {   
+            'person':False,
+            'text':"Multiple persons are existed"
+            }      
+    else:
+        return {   
+        'person':False,
+        'text':"no one is found in picture"
+        }      
 
 
 
