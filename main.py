81817dfeb10bdbd5648f3fc3c60bafda2e196a58
Junaid-Zulfiqar <junaidzulfiqar377@gmail.com>
2022-06-17 17:13:46 +0500

    face_dection

--- main.py
+++ main.py
@@ -31,31 +31,31 @@ def read_root():
     return {"Hello": "World"}
 
 #post function
-@app.post("/person_dectector")
-async def upload_base64(item:Item):
-    mpFaceDetection = mp.solutions.face_detection
-    mpDraw = mp.solutions.drawing_utils
-    faceDetection = mpFaceDetection.FaceDetection(model_selection=2,min_detection_confidence=0.4)
-    encoded_data = item.url.split(',')[1]
-    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    results = faceDetection.process(img)
-    if results.detections:
-        if len(results.detections) == 1:
-          return {   
-            'person':True,
-            'text':"Single person found"
-            }  
-        else:
-          return {   
-            'person':False,
-            'text':"Multiple persons are existed"
-            }      
-    else:
-        return {   
-        'person':False,
-        'text':"no one is found in picture"
-        }      
+# @app.post("/person_dectector")
+# async def upload_base64(item:Item):
+#     mpFaceDetection = mp.solutions.face_detection
+#     mpDraw = mp.solutions.drawing_utils
+#     faceDetection = mpFaceDetection.FaceDetection(model_selection=2,min_detection_confidence=0.4)
+#     encoded_data = item.url.split(',')[1]
+#     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#     results = faceDetection.process(img)
+#     if results.detections:
+#         if len(results.detections) == 1:
+#           return {   
+#             'person':True,
+#             'text':"Single person found"
+#             }  
+#         else:
+#           return {   
+#             'person':False,
+#             'text':"Multiple persons are existed"
+#             }      
+#     else:
+#         return {   
+#         'person':False,
+#         'text':"no one is found in picture"
+#         }      
 
 
 
